cc7cff93df96c28a6d56553533ea680bf7c39600
caleb <camarsha@ncsu.edu>
2022-10-20 18:27:06 -0400

    Close to having chambers integrated.

--- secarlo/secar_build.py
+++ secarlo/secar_build.py
@@ -2,23 +2,110 @@ from . import elements
 from . import geometry
 import pkg_resources
 import pandas as pd
+import numpy as np
 
 # Geometry and optical elements defined in csv file
 beam_pipes = pkg_resources.resource_filename("secarlo", "data/beam_pipes.csv")
 
-df = pd.read_csv(beam_pipes)
+df = pd.read_csv(beam_pipes, keep_default_na=False)
+
+
+def produce_points(filename, unit="in"):
+
+    """CSV file with X and Y points
+
+    :param filename: path to file
+    :param unit: inches by default, so convert to meters
+    :returns: list of tuples -> (x, y)
+
+    """
+
+    print(filename)
+    data = pd.read_csv(filename)
+    points = [(x * 0.0254, y * 0.0254) for x, y in zip(data["X"], data["Y"])]
+    return points
+
+
+def produce_radius(filename, unit="in"):
+    """Read file that has the radius defined
+
+    :param filename:
+    :param unit:
+    :returns:
+
+    """
+    data = pd.read_csv(filename)
+    r = data.iloc[0].to_numpy()[0] * 0.0254
+    return r
+
+
+def produce_rectangle(filename, unit="in"):
+    """Place holder function for now
+
+    :param filename:
+    :param unit:
+    :returns: np.inf
+
+    """
+    print("Rectangle input is not implemented at this time!")
+    return np.inf, np.inf
+
+
+def create_geometry_case(geo_type, geo_file):
+    """
+
+    :param geo_type: string that defines type of object
+    :param geo_file: path to file with info
+    :returns: geometry.Geometry object
+
+    """
+    if geo_type == "points":
+        p = produce_points(geo_file)
+        geo_obj = geometry.Polygon2D(p)
+    elif geo_type == "circle":
+        r = produce_radius(geo_file)
+        geo_obj = geometry.Circle(r)
+    elif geo_type == "rectangle":
+        x_lim, y_lim = produce_rectangle(geo_file)
+        geo_obj = geometry.Rectangle(x_lim, y_lim)
+    else:
+        geo_obj = geometry.Geometry()
+
+    return geo_obj
 
 
 def build_secar(df, maps_dir):
     secar = elements.BeamLine()
+    # build the maps and the z positions
     for f, z, n in zip(df["file"], df["Z"], df["NAME"]):
         secar.add_element(
             maps_dir + f,
             z,
             name=str(n),
         )
-
     secar.create_z_array()
+
+    # now set up the constraints
+
+    geometry_dir = pkg_resources.resource_filename("secarlo", "data")
+    geometry_dir += "/"
+
+    for ele, start_type, start_file, end_type, end_file in zip(
+        secar.elements,
+        df["begin_geo"],
+        df["begin_file"],
+        df["end_geo"],
+        df["end_file"],
+    ):
+        if start_type:
+            start_obj = create_geometry_case(
+                start_type, geometry_dir + start_file
+            )
+            ele.set_start_constraint(start_obj)
+        if end_type:
+            end_obj = create_geometry_case(end_type, geometry_dir + end_file)
+            ele.set_end_constraint(end_obj)
+
     return secar
 
 
--- setup.py
+++ setup.py
@@ -2,24 +2,53 @@
 from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
 
-ext1 = Extension(name='transport',
-                 sources=['secarlo/transport.f90'],
-                 f2py_options=['--quiet'],
-                )
+ext1 = Extension(
+    name="transport",
+    sources=["secarlo/transport.f90"],
+    f2py_options=["--quiet"],
+)
 
-ext2 = Extension(name='constraints',
-                 sources=['secarlo/constraints.f90'],
-                 f2py_options=['--quiet'],
-                )
+ext2 = Extension(
+    name="constraints",
+    sources=["secarlo/constraints.f90"],
+    f2py_options=["--quiet"],
+)
 
-setup(name="SeCarlo",
-      version="0.1.1",
-      description="Use COSY maps to simulate SECAR",
-      author="Caleb Marshall",
-      packages=find_packages(),
-      ext_modules=[ext1, ext2],
-      include_package_data=True,
-      data_files = [
-          ('secarlo/data/', ['secarlo/data/beam_pipes.csv'])
-      ]
-      )
+setup(
+    name="SeCarlo",
+    version="0.1.1",
+    description="Use COSY maps to simulate SECAR",
+    author="Caleb Marshall",
+    packages=find_packages(),
+    ext_modules=[ext1, ext2],
+    include_package_data=True,
+    data_files=[
+        ("secarlo/data/", ["secarlo/data/beam_pipes.csv"]),
+        ("secarlo/data/", ["secarlo/data/q5_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/B6_entrance.csv"]),
+        ("secarlo/data/", ["secarlo/data/B4_exit.csv"]),
+        ("secarlo/data/", ["secarlo/data/q6_q7.csv"]),
+        ("secarlo/data/", ["secarlo/data/q12_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/q8_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/q14_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/B2_entrance.csv"]),
+        ("secarlo/data/", ["secarlo/data/q1_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/Hex2.csv"]),
+        ("secarlo/data/", ["secarlo/data/B3_exit.csv"]),
+        ("secarlo/data/", ["secarlo/data/B4_entrance.csv"]),
+        ("secarlo/data/", ["secarlo/data/pipes_after_b2.csv"]),
+        ("secarlo/data/", ["secarlo/data/q10_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/hex1_q3_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/q2_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/Hex3.csv"]),
+        ("secarlo/data/", ["secarlo/data/q13_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/q9_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/B3_entrance.csv"]),
+        ("secarlo/data/", ["secarlo/data/q4_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/dl_21.csv"]),
+        ("secarlo/data/", ["secarlo/data/q15_chamber.csv"]),
+        ("secarlo/data/", ["secarlo/data/beam_pipes.csv"]),
+        ("secarlo/data/", ["secarlo/data/oct1.csv"]),
+        ("secarlo/data/", ["secarlo/data/q11_chamber.csv"]),
+    ],
+)
